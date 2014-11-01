6e57c2aab76059a59b993d426c594b617832f56a
FlyerDR <wirtha@bu.edu>
2014-11-01 04:47:21 +0000

    Add new helper methods to model classes

--- pointstracker/admin.py
+++ pointstracker/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from pointstracker.models import *
 
-admin.site.register(RewardsProgram)
-admin.site.register(RewardsProgramAccount)
+class RewardsProgramAdmin(admin.ModelAdmin):
+  list_display = ("__unicode__", "shortname", "displayname")
+
+admin.site.register(RewardsProgram, RewardsProgramAdmin)
+
+class RewardsProgramAccountAdmin(admin.ModelAdmin):
+  list_display = ("__unicode__", "user", "rewards_program", "current_balance")
+
+admin.site.register(RewardsProgramAccount, RewardsProgramAccountAdmin)
 
 class RewardsProgramAccountRevisionEntryInline(admin.StackedInline):
   model = RewardsProgramAccountRevisionEntry
--- pointstracker/models.py
+++ pointstracker/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import datetime
+
 class RewardsProgram(models.Model):
   shortname = models.CharField(max_length=32, unique=True)
   displayname = models.CharField(max_length=255)
@@ -12,20 +14,44 @@ class RewardsProgram(models.Model):
 
 class RewardsProgramAccount(models.Model):
   user = models.ForeignKey(User)
-  rewards_program = models.ForeignKey(RewardsProgram)
+  rewards_program = models.ForeignKey(RewardsProgram, related_name="accounts", related_query_name="account")
   created_time = models.DateTimeField(auto_now_add=True, editable=False)
   updated_time = models.DateTimeField(auto_now_add=True, auto_now=True, editable=False)
   cred1 = models.CharField("Credential 1", max_length=255)
   cred2 = models.CharField("Credential 2", max_length=255, null=True)
 
+  def __unicode__(self):
+    return "{0} for {1}".format(self.rewards_program, self.user)
+
+  def current_balance(self):
+    try:
+      return self.revisions.order_by('-revision')[0].get_current_balance()
+    except IndexError:
+      return None
+    
+
 class RewardsProgramAccountRevision(models.Model):
-  account = models.ForeignKey(RewardsProgramAccount)
+  account = models.ForeignKey(RewardsProgramAccount, related_name="revisions", related_query_name="revision")
   pending = models.BooleanField(default=False)
   revision = models.PositiveIntegerField()
   created_time = models.DateTimeField(auto_now_add=True, editable=False)
   updated_time = models.DateTimeField(auto_now_add=True, auto_now=True, editable=False)
 
+  def __unicode__(self):
+    return "{0} rev {1}".format(self.account, self.revision)
+
+  def get_current_balance(self):
+    if self.pending: return None
+    now = datetime.date.today()
+    return sum(x.amount for x in self.entries.all() if x.expiration_date is None or x.expiration_date > now)
+
 class RewardsProgramAccountRevisionEntry(models.Model):
-  revision = models.ForeignKey(RewardsProgramAccountRevision)
+  revision = models.ForeignKey(RewardsProgramAccountRevision, related_name="entries", related_query_name="entry")
   amount = models.PositiveIntegerField()
   expiration_date = models.DateField(null=True)
+
+  def __unicode__(self):
+    s = "{0} points".format(self.amount)
+    if self.expiration_date != None:
+      s += " expires {0}".format(self.expiration_date)
+    return s
