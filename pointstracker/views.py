256edbf0be03cc058096716b63e71e705faaa37d
zhushigang <zhushigang@gmail.com>
2014-12-26 05:02:37 -0500

    Enables users to add reward accounts
    Users can now link reward accounts to their flyerdr account. The reward
    accounts login credentials are saved in databse.

--- pointstracker/views.py
+++ pointstracker/views.py
@@ -40,32 +40,49 @@ def signup(req):
 
 @login_required
 def manage(req):
-  accounts = list(models.RewardsProgramAccount.objects.filter(user=req.user))
-  revs = list()
-  for acc in accounts:
-    revs += [list(models.RewardsProgramAccountRevision.objects.filter(account = acc))]
+  me = req.method
+  po = req.POST
+  if req.method=='GET':
+    accounts = list(models.RewardsProgramAccount.objects.filter(user_id=20))
+    revs = list()
+    for acc in accounts:
+      revs += [list(models.RewardsProgramAccountRevision.objects.filter(account = acc))]
 
-  last_entries = list()
-  for entries in revs:
-    last_entries += [models.RewardsProgramAccountRevisionEntry.objects.filter(revision=entries[-1])]
+    last_entries = list()
+    for entries in revs:
+      if len(entries)>0:
+        last_entries += [models.RewardsProgramAccountRevisionEntry.objects.filter(revision=entries[-1])]
   
-  programs = list(models.RewardsProgram.objects.all())
-  keys = list()
-  for program in programs:
-    keys += [program.shortname]
-  values = list()
-  for entry in last_entries:
-    values +=[entry[0].amount]
+    programs = list(models.RewardsProgram.objects.all())
+    keys = list()
+    for program in programs:
+      keys += [program.shortname]
+    values = list()
+    for entry in last_entries:
+      values +=[entry[0].amount]
   
-  json = dict()
-  for x in range(len(keys)):
-    if x<len(values):
-      json[keys[x]] = values[x]
-    else:
-      json[keys[x]] = "Please add your account"
-
-  return render(req, 'pointstracker/manage.html', json)
-
+    json = dict()
+    for x in range(len(keys)):
+      if x<len(values):
+        json[keys[x]] = values[x]
+      else:
+        json[keys[x]] = "Please add your account"
+    for acc in accounts:
+      shortname = acc.rewards_program.shortname
+      json[shortname] = "Account information not yet updated"
+    return render(req, 'pointstracker/manage.html', json)
+  elif req.method=='POST':
+    if "id" in req.POST and "password" in req.POST:
+      id = req.POST['id']
+      password = req.POST['password']
+      program_shortname = req.POST['shortname']
+      program = models.RewardsProgram.objects.get(shortname=program_shortname)
+      value_pairs = {'cred1':id,'cred2':password}
+      new_RewardsProgramAccount, created = models.RewardsProgramAccount.objects.update_or_create(user=req.user, rewards_program=program,defaults=value_pairs)
+      new_RewardsProgramAccount.save()
+    return redirect('manage')
+	
+	
 def about(req):
   return render(req, 'pointstracker/about.html', {})
 
